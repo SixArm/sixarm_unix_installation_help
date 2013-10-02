43cd560c80936955d20d7d3cce2c1644835c0e89
SixArm <sixarm@sixarm.com>
2013-10-01 21:00:49 -0700

    Improve creation of directories

--- webcache.sh
+++ webcache.sh
@@ -1,13 +1,11 @@
-cd ~/webcache || exit 1
-mkdir cs
-mkdir cs/ruby
-mkdir cs/ruby/mocha
+mkdir -p ~/webcache
 
-wget-mirror cs/ruby/mocha http://gofreerange.com/mocha/docs/
-
-cd cs/ruby
+mkdir -p ~/webcache/cs/ruby && cd $_ || exit 1
 curl -s http://www.ruby-doc.org/downloads/ruby_2_0_core_rdocs.tgz | tar zxf -
 curl -s http://www.ruby-doc.org/downloads/learning-ruby.tar.gz | tar zxf -
 curl -s http://www.ruby-doc.org/downloads/ruby_2_0_stdlib_rdocs.tgz | tar zxf -
 curl -s http://mattsears.com/articles/2011/12/10/minitest-quick-reference -o minitest-quick-reference-by-matt-sears.html
+
+mkdir -p ~/webcache/cs/ruby/mocha && cd $_ || exit 1
+wget-mirror ~/webcache/cs/ruby/mocha http://gofreerange.com/mocha/docs/
 
