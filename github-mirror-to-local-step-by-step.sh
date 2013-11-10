51a95bf84f20ea9bc04604cd679f975b79ab8f01
SixArm <sixarm@sixarm.com>
2013-11-10 00:39:41 -0500

    Raise directory and clone shallow

--- github-mirror-to-local-step-by-step.sh
+++ github-mirror-to-local-step-by-step.sh
@@ -1,19 +1,16 @@
 ## GitHub mirror to local system, step by step
 
-mkdir -p /opt/github && cd /opt/github
+mkdir -p /opt && cd /opt
 
-mkdir rails
-git clone git://github.com/rails/rails.git rails/rails
-git clone git://github.com/rails/arel.git rails/arel
-git clone git://github.com/rails/journey.git rails/journey
-git clone git://github.com/rails/sprockets-rails.git rails/sprockets-rails
-git clone git://github.com/rails/sass-rails.git rails/sass-rails
-git clone git://github.com/rails/coffee-rails.git rails/coffee-rails
-git clone git://github.com/rails/activerecord-deprecated_finders.git rails/activerecord-deprecated_finders
+git clone --depth 1 git://github.com/rails/rails.git
+git clone --depth 1 git://github.com/rails/arel.git
+git clone --depth 1 git://github.com/rails/journey.git
+git clone --depth 1 git://github.com/rails/sprockets-rails.git
+git clone --depth 1 git://github.com/rails/sass-rails.git
+git clone --depth 1 git://github.com/rails/coffee-rails.git
+git clone --depth 1 git://github.com/rails/activerecord-deprecated_finders.git
 
-mkdir sstephenson
-git clone git://github.com/sstephenson/ruby-build.git sstephenson/ruby-build
-git clone git://github.com/sstephenson/rbenv.git sstephenson/rbenv
-
+git clone --depth 1 git://github.com/sstephenson/ruby-build.git
+git clone --depth 1 git://github.com/sstephenson/rbenv.git
 
 
