b532899d3851390d0d342a9f17e89888c078e76a
SixArm <sixarm@sixarm.com>
2013-11-10 00:44:05 -0500

    DEPRECATED

--- github-mirror-to-local-step-by-step.sh
+++ github-mirror-to-local-step-by-step.sh
@@ -1,16 +1,1 @@
-## GitHub mirror to local system, step by step
-
-mkdir -p /opt && cd /opt
-
-git clone --depth 1 git://github.com/rails/rails.git
-git clone --depth 1 git://github.com/rails/arel.git
-git clone --depth 1 git://github.com/rails/journey.git
-git clone --depth 1 git://github.com/rails/sprockets-rails.git
-git clone --depth 1 git://github.com/rails/sass-rails.git
-git clone --depth 1 git://github.com/rails/coffee-rails.git
-git clone --depth 1 git://github.com/rails/activerecord-deprecated_finders.git
-
-git clone --depth 1 git://github.com/sstephenson/ruby-build.git
-git clone --depth 1 git://github.com/sstephenson/rbenv.git
-
-
+DEPRECATED
