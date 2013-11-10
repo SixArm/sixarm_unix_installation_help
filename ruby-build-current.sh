dcc59f2f956703bf6e4b41eac7c832fc8bc10ba3
SixArm <sixarm@sixarm.com>
2013-11-10 00:48:13 -0500

    Make more flexible

--- ruby-build-current.sh
+++ ruby-build-current.sh
@@ -1,5 +1,5 @@
 sudo su
 mkdir -p /opt/ruby
-ruby-build --definitions | grep "^1.9.3-p[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
+ruby-build --definitions | grep "^2\.[0-9]\.[0-9]-p[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
 cd /opt/ruby
 ln -s 1.9.3* current
