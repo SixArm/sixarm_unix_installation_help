1d02fbaaf58f8ff5d2970599091f5b8b9ed87f11
SixArm <sixarm@sixarm.com>
2014-05-29 01:23:05 -0700

    Add updating

--- ruby-build-current.sh
+++ ruby-build-current.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
-# Install the current Ruby MRI by using ruby-build.
-# We put ruby in /opt/ruby/x.y.z and link it to /opt/ruby/current.
+# Install the current Ruby MRI by using ruby-build
+# and our preferred setup:
 #
+#   * Use /opt/ruby-build and git pull and install.sh to update it.
+#   * Install ruby in /opt/ruby/x.y.z  and symlink /opt/ruby/current.
+
 sudo su -
 mkdir -p /opt/ruby
+cd /opt/ruby-build && git pull && ./install.sh
 ruby-build --definitions | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
 cd /opt/ruby
 ln -t1 | head -1 | xargs -I{} ln -s "{}" current
