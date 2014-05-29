3dc91362be7d2cf9cd3942fd27ef9564cacdde13
SixArm <sixarm@sixarm.com>
2014-05-29 01:17:18 -0700

    Update for new Ruby verion format X.Y.Z

--- ruby-build-current.sh
+++ ruby-build-current.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+#
+# Install the current Ruby MRI by using ruby-build.
+# We put ruby in /opt/ruby/x.y.z and link it to /opt/ruby/current.
+#
 sudo su -
 mkdir -p /opt/ruby
-ruby-build --definitions | grep "^2\.[0-9]\.[0-9]-p[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
+ruby-build --definitions | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
 cd /opt/ruby
-ln -s 2.* current
+ln -t1 | head -1 | xargs -I{} ln -s "{}" current
