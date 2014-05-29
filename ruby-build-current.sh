92a59cc1e97b6297aa6b3c7ec77907b634dbe83c
SixArm <sixarm@sixarm.com>
2014-05-29 01:32:17 -0700

    Improve version semantics

--- ruby-build-current.sh
+++ ruby-build-current.sh
@@ -7,8 +7,17 @@
 #   * Install ruby in /opt/ruby/x.y.z  and symlink /opt/ruby/current.
 
 sudo su -
-mkdir -p /opt/ruby
-cd /opt/ruby-build && git pull && ./install.sh
-ruby-build --definitions | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | tail -1 | xargs -I{} ruby-build --verbose {} /opt/ruby/{}
-cd /opt/ruby
-ln -t1 | head -1 | xargs -I{} ln -s "{}" current
+
+# Update ruby definitions
+cd /opt/ruby-build &&
+git pull &&
+./install.sh
+
+# Get the current MRI version,such as X.Y.Z
+VERSION=`ruby-build --definitions | grep "^[0-9]\+\.[0-9]\+\.[0-9]\+$" | tail -1`
+
+# Build it
+RUBY_DIR=/opt/ruby
+ruby-build --verbose $VERSION $RUBY_DIR/$VERSION && 
+cd $RUBY_DIR &&
+ln -s $VERSION current
