8db42f96d3f2c3ae80567e876dface8adfa73f02
SixArm <sixarm@sixarm.com>
2014-12-09 21:38:46 -0700

    Update and rename java-8.sh to java.sh

--- java-8.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
---
+++ java.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+
+# Installing Java 8 should take care of updating alternativates.
+# Just in case, here are the commands you can run manually.
+#sudo update-alternatives --config java
+#sudo update-alternatives --config javac
