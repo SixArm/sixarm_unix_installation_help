ba2175eafb55675019536955a3f24df274513ef4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-10-28 22:05:34 -0600

    Add top

--- git-clone-repos-we-want.sh
+++ git-clone-repos-we-want.sh
@@ -1,11 +1,13 @@
-cd /opt
-git clone https://github.com/SixArm/sixarm_debian_dpkg_scripts.git
-git clone https://github.com/SixArm/sixarm_htx.git
-git clone https://github.com/SixArm/sixarm_gem_scripts.git
-git clone https://github.com/SixArm/sixarm_git_gitconfig.git
-git clone https://github.com/SixArm/sixarm_mysql_scripts.git
-git clone https://github.com/SixArm/sixarm_postgresql_scripts.git
-git clone https://github.com/SixArm/sixarm_ruby_gem_scripts.git
-git clone https://github.com/SixArm/sixarm_ruby_gemforge.git
-git clone https://github.com/SixArm/sixarm_strategem.git
-git clone https://github.com/SixArm/sixarm_unix_shell_scripts.git
+top=/opt
+cd $top
+git clone https://github.com/SixArm/sixarm_debian_dpkg_scripts
+git clone https://github.com/SixArm/sixarm_htx
+git clone https://github.com/SixArm/sixarm_gem_scripts
+git clone https://github.com/SixArm/sixarm_git_gitconfig
+git clone https://github.com/SixArm/sixarm_mysql_scripts
+git clone https://github.com/SixArm/sixarm_postgresql_scripts
+git clone https://github.com/SixArm/sixarm_ruby_gem_scripts
+git clone https://github.com/SixArm/sixarm_ruby_gemforge
+git clone https://github.com/SixArm/sixarm_strategem
+git clone https://github.com/SixArm/sixarm_unix_shell_scripts
+git clone https://github.com/jpsim/sourcekitten; cd sourcekitten && git checkout swift1p2 && make install; cd $top
